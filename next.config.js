ce4ab72c95bf8de39172ce19f20a0aa52e140993
tyapiko <93905845+tyapiko@users.noreply.github.com>
2025-12-07 21:51:08 +0900

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-    NEXT_PUBLIC_GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
-    PICA_SECRET_KEY: process.env.PICA_SECRET_KEY,
-    PICA_NOTION_CONNECTION_KEY: process.env.PICA_NOTION_CONNECTION_KEY,
-    SERVICE_ROLE_KEY: process.env.SERVICE_ROLE_KEY,
-  },
 }
 
 module.exports = nextConfig
